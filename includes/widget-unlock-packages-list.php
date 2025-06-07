db1de9b229a67654c59d738bd8fa9b940d57fd55
Steeven Lacerna <steeven93@icloud.com>
2025-06-07 15:22:33 +0400

    feat(widget): add style controls for packages list widget
    Add comprehensive styling options including typography, colors, spacing, and hover effects for all elements in the packages list widget. Enable inline editing for dynamic content customization.

--- includes/widget-unlock-packages-list.php
+++ includes/widget-unlock-packages-list.php
@@ -121,26 +121,391 @@ class Unlock_Widget_Packages_List extends \Elementor\Widget_Base {
 		);
 
 		$this->end_controls_section();
+
+		// Style Tab
+		$this->start_controls_section(
+			'section_style',
+			[
+				'label' => __( 'Style', 'unlock-elementor-widgets' ),
+				'tab' => \Elementor\Controls_Manager::TAB_STYLE,
+			]
+		);
+
+		$this->add_group_control(
+			\Elementor\Group_Control_Typography::get_type(),
+			[
+				'name' => 'title_typography',
+				'label' => __( 'Title Typography', 'unlock-elementor-widgets' ),
+				'selector' => '{{WRAPPER}} .unlock-heading',
+			]
+		);
+
+		$this->add_control(
+			'title_color',
+			[
+				'label' => __( 'Title Color', 'unlock-elementor-widgets' ),
+				'type' => \Elementor\Controls_Manager::COLOR,
+				'selectors' => [
+					'{{WRAPPER}} .unlock-heading' => 'color: {{VALUE}}',
+				],
+			]
+		);
+
+		$this->add_responsive_control(
+			'title_margin',
+			[
+				'label' => __( 'Title Margin', 'unlock-elementor-widgets' ),
+				'type' => \Elementor\Controls_Manager::DIMENSIONS,
+				'size_units' => [ 'px', '%', 'em' ],
+				'selectors' => [
+					'{{WRAPPER}} .unlock-heading' => 'margin: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+				],
+			]
+		);
+
+		$this->add_control(
+			'card_style_heading',
+			[
+				'label' => __( 'Package Card', 'unlock-elementor-widgets' ),
+				'type' => \Elementor\Controls_Manager::HEADING,
+				'separator' => 'before',
+			]
+		);
+
+		$this->add_group_control(
+			\Elementor\Group_Control_Background::get_type(),
+			[
+				'name' => 'card_background',
+				'label' => __( 'Card Background', 'unlock-elementor-widgets' ),
+				'types' => [ 'classic', 'gradient' ],
+				'selector' => '{{WRAPPER}} .unlock-package-card',
+			]
+		);
+
+		$this->add_responsive_control(
+			'card_padding',
+			[
+				'label' => __( 'Card Padding', 'unlock-elementor-widgets' ),
+				'type' => \Elementor\Controls_Manager::DIMENSIONS,
+				'size_units' => [ 'px', '%', 'em' ],
+				'selectors' => [
+					'{{WRAPPER}} .unlock-package-card' => 'padding: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+				],
+			]
+		);
+
+		$this->add_group_control(
+			\Elementor\Group_Control_Border::get_type(),
+			[
+				'name' => 'card_border',
+				'label' => __( 'Card Border', 'unlock-elementor-widgets' ),
+				'selector' => '{{WRAPPER}} .unlock-package-card',
+			]
+		);
+
+		$this->add_control(
+			'card_border_radius',
+			[
+				'label' => __( 'Border Radius', 'unlock-elementor-widgets' ),
+				'type' => \Elementor\Controls_Manager::DIMENSIONS,
+				'size_units' => [ 'px', '%' ],
+				'selectors' => [
+					'{{WRAPPER}} .unlock-package-card' => 'border-radius: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+				],
+			]
+		);
+
+		$this->add_group_control(
+			\Elementor\Group_Control_Box_Shadow::get_type(),
+			[
+				'name' => 'card_box_shadow',
+				'label' => __( 'Card Box Shadow', 'unlock-elementor-widgets' ),
+				'selector' => '{{WRAPPER}} .unlock-package-card',
+			]
+		);
+
+		// Package Name Styling
+		$this->add_control(
+			'pkg_name_style_heading',
+			[
+				'label' => __( 'Package Name', 'unlock-elementor-widgets' ),
+				'type' => \Elementor\Controls_Manager::HEADING,
+				'separator' => 'before',
+			]
+		);
+
+		$this->add_group_control(
+			\Elementor\Group_Control_Typography::get_type(),
+			[
+				'name' => 'pkg_name_typography',
+				'label' => __( 'Typography', 'unlock-elementor-widgets' ),
+				'selector' => '{{WRAPPER}} .unlock-package-name',
+			]
+		);
+
+		$this->add_control(
+			'pkg_name_color',
+			[
+				'label' => __( 'Color', 'unlock-elementor-widgets' ),
+				'type' => \Elementor\Controls_Manager::COLOR,
+				'selectors' => [
+					'{{WRAPPER}} .unlock-package-name' => 'color: {{VALUE}}',
+				],
+			]
+		);
+
+		// Package Description Styling
+		$this->add_control(
+			'pkg_desc_style_heading',
+			[
+				'label' => __( 'Package Description', 'unlock-elementor-widgets' ),
+				'type' => \Elementor\Controls_Manager::HEADING,
+				'separator' => 'before',
+			]
+		);
+
+		$this->add_group_control(
+			\Elementor\Group_Control_Typography::get_type(),
+			[
+				'name' => 'pkg_desc_typography',
+				'label' => __( 'Typography', 'unlock-elementor-widgets' ),
+				'selector' => '{{WRAPPER}} .unlock-package-desc',
+			]
+		);
+
+		$this->add_control(
+			'pkg_desc_color',
+			[
+				'label' => __( 'Color', 'unlock-elementor-widgets' ),
+				'type' => \Elementor\Controls_Manager::COLOR,
+				'selectors' => [
+					'{{WRAPPER}} .unlock-package-desc' => 'color: {{VALUE}}',
+				],
+			]
+		);
+
+		// Package Price Styling
+		$this->add_control(
+			'pkg_price_style_heading',
+			[
+				'label' => __( 'Package Price', 'unlock-elementor-widgets' ),
+				'type' => \Elementor\Controls_Manager::HEADING,
+				'separator' => 'before',
+			]
+		);
+
+		$this->add_group_control(
+			\Elementor\Group_Control_Typography::get_type(),
+			[
+				'name' => 'pkg_price_typography',
+				'label' => __( 'Typography', 'unlock-elementor-widgets' ),
+				'selector' => '{{WRAPPER}} .unlock-package-price',
+			]
+		);
+
+		$this->add_control(
+			'pkg_price_color',
+			[
+				'label' => __( 'Color', 'unlock-elementor-widgets' ),
+				'type' => \Elementor\Controls_Manager::COLOR,
+				'selectors' => [
+					'{{WRAPPER}} .unlock-package-price' => 'color: {{VALUE}}',
+				],
+			]
+		);
+
+		// Package Features Styling
+		$this->add_control(
+			'pkg_features_style_heading',
+			[
+				'label' => __( 'Package Features', 'unlock-elementor-widgets' ),
+				'type' => \Elementor\Controls_Manager::HEADING,
+				'separator' => 'before',
+			]
+		);
+
+		$this->add_group_control(
+			\Elementor\Group_Control_Typography::get_type(),
+			[
+				'name' => 'pkg_features_typography',
+				'label' => __( 'Typography', 'unlock-elementor-widgets' ),
+				'selector' => '{{WRAPPER}} .unlock-package-card ul li',
+			]
+		);
+
+		$this->add_control(
+			'pkg_features_color',
+			[
+				'label' => __( 'Color', 'unlock-elementor-widgets' ),
+				'type' => \Elementor\Controls_Manager::COLOR,
+				'selectors' => [
+					'{{WRAPPER}} .unlock-package-card ul li' => 'color: {{VALUE}}',
+				],
+			]
+		);
+
+		// Button Styling
+		$this->add_control(
+			'button_style_heading',
+			[
+				'label' => __( 'Button', 'unlock-elementor-widgets' ),
+				'type' => \Elementor\Controls_Manager::HEADING,
+				'separator' => 'before',
+			]
+		);
+
+		$this->add_group_control(
+			\Elementor\Group_Control_Typography::get_type(),
+			[
+				'name' => 'button_typography',
+				'label' => __( 'Typography', 'unlock-elementor-widgets' ),
+				'selector' => '{{WRAPPER}} .unlock-buy-btn',
+			]
+		);
+
+		$this->start_controls_tabs( 'button_tabs' );
+
+		$this->start_controls_tab(
+			'button_normal_tab',
+			[
+				'label' => __( 'Normal', 'unlock-elementor-widgets' ),
+			]
+		);
+
+		$this->add_control(
+			'button_text_color',
+			[
+				'label' => __( 'Text Color', 'unlock-elementor-widgets' ),
+				'type' => \Elementor\Controls_Manager::COLOR,
+				'selectors' => [
+					'{{WRAPPER}} .unlock-buy-btn' => 'color: {{VALUE}};',
+				],
+			]
+		);
+
+		$this->add_group_control(
+			\Elementor\Group_Control_Background::get_type(),
+			[
+				'name' => 'button_background',
+				'label' => __( 'Background', 'unlock-elementor-widgets' ),
+				'types' => [ 'classic', 'gradient' ],
+				'selector' => '{{WRAPPER}} .unlock-buy-btn',
+			]
+		);
+
+		$this->add_group_control(
+			\Elementor\Group_Control_Border::get_type(),
+			[
+				'name' => 'button_border',
+				'label' => __( 'Border', 'unlock-elementor-widgets' ),
+				'selector' => '{{WRAPPER}} .unlock-buy-btn',
+			]
+		);
+
+		$this->add_control(
+			'button_border_radius',
+			[
+				'label' => __( 'Border Radius', 'unlock-elementor-widgets' ),
+				'type' => \Elementor\Controls_Manager::DIMENSIONS,
+				'size_units' => [ 'px', '%' ],
+				'selectors' => [
+					'{{WRAPPER}} .unlock-buy-btn' => 'border-radius: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+				],
+			]
+		);
+
+		$this->add_responsive_control(
+			'button_padding',
+			[
+				'label' => __( 'Padding', 'unlock-elementor-widgets' ),
+				'type' => \Elementor\Controls_Manager::DIMENSIONS,
+				'size_units' => [ 'px', 'em', '%' ],
+				'selectors' => [
+					'{{WRAPPER}} .unlock-buy-btn' => 'padding: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+				],
+			]
+		);
+
+		$this->end_controls_tab();
+
+		$this->start_controls_tab(
+			'button_hover_tab',
+			[
+				'label' => __( 'Hover', 'unlock-elementor-widgets' ),
+			]
+		);
+
+		$this->add_control(
+			'button_hover_text_color',
+			[
+				'label' => __( 'Text Color', 'unlock-elementor-widgets' ),
+				'type' => \Elementor\Controls_Manager::COLOR,
+				'selectors' => [
+					'{{WRAPPER}} .unlock-buy-btn:hover' => 'color: {{VALUE}};',
+				],
+			]
+		);
+
+		$this->add_group_control(
+			\Elementor\Group_Control_Background::get_type(),
+			[
+				'name' => 'button_hover_background',
+				'label' => __( 'Background', 'unlock-elementor-widgets' ),
+				'types' => [ 'classic', 'gradient' ],
+				'selector' => '{{WRAPPER}} .unlock-buy-btn:hover',
+			]
+		);
+
+		$this->add_group_control(
+			\Elementor\Group_Control_Border::get_type(),
+			[
+				'name' => 'button_hover_border',
+				'label' => __( 'Border', 'unlock-elementor-widgets' ),
+				'selector' => '{{WRAPPER}} .unlock-buy-btn:hover',
+			]
+		);
+
+		$this->add_control(
+			'button_hover_border_radius',
+			[
+				'label' => __( 'Border Radius', 'unlock-elementor-widgets' ),
+				'type' => \Elementor\Controls_Manager::DIMENSIONS,
+				'size_units' => [ 'px', '%' ],
+				'selectors' => [
+					'{{WRAPPER}} .unlock-buy-btn:hover' => 'border-radius: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+				],
+			]
+		);
+
+		$this->end_controls_tab();
+		$this->end_controls_tabs();
+
+		$this->end_controls_section(); // End Style Tab
 	}
 
 	protected function render() {
 		$settings = $this->get_settings_for_display();
 		?>
 		<div class="unlock-packages-wrapper">
-			<h3 class="unlock-heading"><?php echo esc_html( $settings['list_title'] ); ?></h3>
+			<h3 class="unlock-heading elementor-inline-editing" data-elementor-setting-key="list_title" data-elementor-inline-editing-toolbar="basic"><?php echo esc_html( $settings['list_title'] ); ?></h3>
 			<div id="unlock-packages-list">
 				<?php if ( \Elementor\Plugin::$instance->editor->is_edit_mode() ) : ?>
 					<?php if ( ! empty( $settings['packages_list_items'] ) ) : ?>
-						<?php foreach ( $settings['packages_list_items'] as $index => $item ) : ?>
-							<div class="unlock-package-card" data-id="demo-<?php echo esc_attr( $index ); ?>">
+						<?php foreach ( $settings['packages_list_items'] as $index => $item ) : 
+							$repeater_setting_key = $this->get_repeater_setting_key( 'pkg_name', 'packages_list_items', $index );
+							$repeater_desc_key = $this->get_repeater_setting_key( 'pkg_description', 'packages_list_items', $index );
+							$repeater_price_key = $this->get_repeater_setting_key( 'pkg_price', 'packages_list_items', $index );
+							$repeater_features_key = $this->get_repeater_setting_key( 'pkg_features', 'packages_list_items', $index );
+						?>
+							<div class="unlock-package-card elementor-repeater-item-<?php echo esc_attr( $item['_id'] ); ?>" data-id="demo-<?php echo esc_attr( $index ); ?>">
 								<?php if ( ! empty( $item['pkg_image']['url'] ) ) : ?>
 									<img src="<?php echo esc_url( $item['pkg_image']['url'] ); ?>" alt="<?php echo esc_attr( $item['pkg_name'] ); ?>" class="unlock-package-image" style="width:100%;max-width:150px;height:auto;margin-bottom:10px;">
 								<?php endif; ?>
-								<h4 class="unlock-package-name"><?php echo esc_html( $item['pkg_name'] ); ?></h4>
-								<p class="unlock-package-desc"><?php echo nl2br( esc_html( $item['pkg_description'] ) ); ?></p>
-								<p class="unlock-package-price"><?php echo esc_html( $item['pkg_price'] ); ?></p>
+								<h4 class="unlock-package-name elementor-inline-editing" data-elementor-setting-key="<?php echo esc_attr( $repeater_setting_key ); ?>" data-elementor-inline-editing-toolbar="basic"><?php echo esc_html( $item['pkg_name'] ); ?></h4>
+								<div class="unlock-package-desc elementor-inline-editing" data-elementor-setting-key="<?php echo esc_attr( $repeater_desc_key ); ?>" data-elementor-inline-editing-toolbar="advanced"><?php echo nl2br( $item['pkg_description'] ); ?></div>
+								<div class="unlock-package-price elementor-inline-editing" data-elementor-setting-key="<?php echo esc_attr( $repeater_price_key ); ?>" data-elementor-inline-editing-toolbar="basic"><?php echo esc_html( $item['pkg_price'] ); ?></div>
 								<?php if ( ! empty( $item['pkg_features'] ) ) : ?>
-									<ul>
+									<ul class="elementor-inline-editing" data-elementor-setting-key="<?php echo esc_attr( $repeater_features_key ); ?>" data-elementor-inline-editing-toolbar="advanced">
 										<?php
 										$features = explode( "\n", $item['pkg_features'] );
 										foreach ( $features as $feature ) :
