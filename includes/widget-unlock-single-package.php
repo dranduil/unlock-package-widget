d82dad70b0ef8cdb8fd04fd9263c23cb134e66c7
Steeven Lacerna <steeven93@icloud.com>
2025-06-07 15:08:08 +0400

    feat(widget): add demo package preview in editor mode
    Display a demo package with sample data when the widget is viewed in Elementor's editor mode to provide a better editing experience

--- includes/widget-unlock-single-package.php
+++ includes/widget-unlock-single-package.php
@@ -51,7 +51,25 @@ class Unlock_Widget_Single_Package extends \Elementor\Widget_Base {
 		<div class="unlock-single-package-wrapper">
 			<h3 class="unlock-heading"><?php esc_html_e( 'Dettaglio Pacchetto', 'unlock-elementor-widgets' ); ?></h3>
 			<div id="unlock-single-package-<?php echo $pkg_id; ?>" data-package-id="<?php echo $pkg_id; ?>">
-				<p><?php esc_html_e( 'Caricamento dettagli…', 'unlock-elementor-widgets' ); ?></p>
+				<?php if ( \Elementor\Plugin::$instance->editor->is_edit_mode() ) : ?>
+					<?php
+					$demo_pkg = ['id' => $pkg_id ?: 'demo', 'name' => 'Demo Single Package', 'description' => 'Detailed description of this amazing demo package.', 'price' => 'AED 25', 'features' => ['Exclusive Feature X', 'Exclusive Feature Y'], 'image_url' => 'https://via.placeholder.com/250'];
+					?>
+					<div class="unlock-package-card" data-id="<?php echo esc_attr( $demo_pkg['id'] ); ?>">
+                        <img src="<?php echo esc_url( $demo_pkg['image_url'] ); ?>" alt="<?php echo esc_attr( $demo_pkg['name'] ); ?>" class="unlock-package-image" style="width:100%;max-width:250px;height:auto;margin-bottom:10px;">
+						<h4 class="unlock-package-name"><?php echo esc_html( $demo_pkg['name'] ); ?></h4>
+						<p class="unlock-package-desc"><?php echo esc_html( $demo_pkg['description'] ); ?></p>
+						<p class="unlock-package-price"><?php echo esc_html( $demo_pkg['price'] ); ?></p>
+						<ul>
+							<?php foreach ( $demo_pkg['features'] as $feature ) : ?>
+								<li><?php echo esc_html( $feature ); ?></li>
+							<?php endforeach; ?>
+						</ul>
+						<button class="unlock-buy-btn" data-id="<?php echo esc_attr( $demo_pkg['id'] ); ?>">Acquista</button>
+					</div>
+				<?php else : ?>
+					<p><?php esc_html_e( 'Caricamento dettagli…', 'unlock-elementor-widgets' ); ?></p>
+				<?php endif; ?>
 			</div>
 		</div>
 		<?php
